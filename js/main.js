655e9d65ea79c3cb0748aba65f67827c069b4e1d
Florent Douine <florent@douine.fr>
2021-05-03 13:56:29 +0200

    Continue first page

--- js/main.js
+++ js/main.js
@@ -15,3 +15,30 @@ function setHeaderOpacity() {
     var alpha = 1 - (menuHeight-pageYOffset)/menuHeight;
     $("#header-slider").css("background-color", "rgba(235, 235, 235, "+alpha+")");
 }
+
+ScrollReveal().reveal('#main-title h1:first-child', { 
+    distance: '30px' ,
+    origin: "left",
+    duration: "1000",
+    easing: "ease-out"
+});
+
+ScrollReveal().reveal('#main-title h1:nth-child(2)', { 
+    distance: '30px',
+    delay: 100,
+    origin: "right",
+    duration: "1000",
+    easing: "ease-out"
+});
+
+var rellax = new Rellax('.cmp-number-watermark', {
+    speed: 1,
+});
+
+ScrollReveal().reveal('.anim-bottom-apparition', { 
+    distance: '15px',
+    opacity: 0,
+    origin: "bottom",
+    duration: "1000",
+    easing: "linear"
+});
