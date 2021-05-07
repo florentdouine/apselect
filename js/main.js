2258ad895ee3e2029c0ee9655fb479085a669ac3
Florent Douine <florent@douine.fr>
2021-05-07 23:55:06 +0200

    Continue homepage

--- js/main.js
+++ js/main.js
@@ -1,6 +1,39 @@
 window.addEventListener('scroll', function() {
-    setHeaderOpacity();
+    windowScreenChanged() 
+    //setHeaderOpacity();
 });
+
+window.addEventListener('resize', function(event) {
+    windowScreenChanged() 
+}, true);
+
+windowScreenChanged();
+
+function windowScreenChanged() {
+    document.querySelectorAll(".anim-background-inversion").forEach(function(element) {
+        if(startToAppear(element)) {
+            document.getElementsByTagName("body")[0].style.backgroundColor = "white"
+        } else {
+            document.getElementsByTagName("body")[0].style.backgroundColor = "black"
+        }
+    });
+}
+
+// var startToBeVisible = elTop < window.innerHeight
+// var startToEnd = elTop + elHeight - window.innerHeight < 0
+// var stopToBeVisible = elTop + elHeight < 0
+function startToAppear(el) {
+    var elTop = el.getBoundingClientRect().top
+    console.log("elTop", elTop, "window.innerHeight", window.innerHeight)
+    return elTop < window.innerHeight
+}
+
+function startToDisappear(el, margin) {
+    var elTop = el.getBoundingClientRect().top
+    var elHeight = el.offsetHeight
+
+    return elTop + elHeight < window.innerHeight * margin / 100
+}
 
 function toggleMenu() {
     $("nav").toggleClass("closed")
@@ -16,23 +49,19 @@ function setHeaderOpacity() {
     $("#header-slider").css("background-color", "rgba(235, 235, 235, "+alpha+")");
 }
 
-ScrollReveal().reveal('#main-title h1:first-child', { 
+ScrollReveal().reveal('.anim-left-apparition', { 
     distance: '30px' ,
     origin: "left",
     duration: "1000",
     easing: "ease-out"
 });
 
-ScrollReveal().reveal('#main-title h1:nth-child(2)', { 
+ScrollReveal().reveal('.anim-right-apparition', { 
     distance: '30px',
     delay: 100,
     origin: "right",
     duration: "1000",
     easing: "ease-out"
-});
-
-var rellax = new Rellax('.cmp-number-watermark', {
-    speed: 1,
 });
 
 ScrollReveal().reveal('.anim-bottom-apparition', { 
@@ -41,4 +70,8 @@ ScrollReveal().reveal('.anim-bottom-apparition', {
     origin: "bottom",
     duration: "1000",
     easing: "linear"
+});
+
+var rellax = new Rellax('.anim-parallax', {
+    speed: 1,
 });
