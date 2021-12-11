ca8b6bc862944f48d1a87f2c9531604b4b49a862
Flo <fdouine@groupeseb.com>
2021-12-11 13:31:38 +0100

    new images & cookies banner

--- js/main.js
+++ js/main.js
@@ -223,3 +223,59 @@ ScrollReveal().reveal('.anim-bottom-apparition', {
     duration: "1000",
     easing: "linear"
 });
+
+// Cookies
+function checkCookiesStatus() {
+    var status = getCookie("cookiesAccepted")
+    if (status == "") {
+        return
+    }
+
+    document.getElementById("cookies-banner").style.display = "none";
+    
+    if (status == "true") {
+        startGA()
+    }
+}
+
+checkCookiesStatus();
+
+function acceptCookies() {
+    setCookie("cookiesAccepted", "true")
+    checkCookiesStatus()
+}
+
+function refuseCookies() {
+    setCookie("cookiesAccepted", "false")
+    checkCookiesStatus()
+}
+
+function startGA() {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-139459528-1');
+}
+
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
